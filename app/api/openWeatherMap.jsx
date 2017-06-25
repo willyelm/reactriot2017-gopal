55ddf854d802f42ca20bc62162f063ff9fbdae15
Gopal <gopal5388@gmail.com>
2017-06-25 22:48:24 +0530

    updating message

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=Metric';
 
 module.exports = {
   getTemp: function (location) {
